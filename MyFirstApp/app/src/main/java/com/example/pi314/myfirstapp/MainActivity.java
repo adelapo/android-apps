1f3ff12ec77a13cd090068c6ca3ca7f00cdf977f
adelapo <adelapo@berkeley.edu>
2019-04-13 00:55:47 -0700

    add currency conversion in the UI

--- MyFirstApp/app/src/main/java/com/example/pi314/myfirstapp/MainActivity.java
+++ MyFirstApp/app/src/main/java/com/example/pi314/myfirstapp/MainActivity.java
@@ -5,17 +5,23 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements AsyncResponse {
 
     JSONObject exchangeRates;
+
+    Spinner spinner1, spinner2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +34,11 @@ public class MainActivity extends AppCompatActivity implements AsyncResponse {
         final Button refreshButton = findViewById(R.id.refreshButton);
 
         final SyncManager syncManager = new SyncManager(); // Keeps track of when text is edited
+
+        new GetExchangeRate(this).execute();
+
+        spinner1 = findViewById(R.id.spinner1);
+        spinner2 = findViewById(R.id.spinner2);
 
         moneyInput1.addTextChangedListener(new TextWatcher() {
             @Override
@@ -44,7 +55,19 @@ public class MainActivity extends AppCompatActivity implements AsyncResponse {
             public void afterTextChanged(Editable s) {
                 if (!syncManager.editingText) {
                     syncManager.editingText = true;
-                    moneyInput2.setText(s);
+
+                    double amount;
+                    if (s.toString().equals("")) {
+                        amount = 0;
+                    } else {
+                        amount = Double.parseDouble(s.toString());
+                    }
+                    String fromCurrency = spinner1.getSelectedItem().toString();
+                    String toCurrency = spinner2.getSelectedItem().toString();
+
+                    double convertedAmount = makeConversion(fromCurrency, toCurrency, amount);
+
+                    moneyInput2.setText(Double.toString(convertedAmount));
                     syncManager.editingText = false;
                 }
             }
@@ -65,7 +88,20 @@ public class MainActivity extends AppCompatActivity implements AsyncResponse {
             public void afterTextChanged(Editable s) {
                 if (!syncManager.editingText) {
                     syncManager.editingText = true;
-                    moneyInput1.setText(s);
+
+                    double amount;
+                    if (s.toString().equals("")) {
+                        amount = 0;
+                    } else {
+                        amount = Double.parseDouble(s.toString());
+                    }
+                    String fromCurrency = spinner1.getSelectedItem().toString();
+                    String toCurrency = spinner2.getSelectedItem().toString();
+
+                    double convertedAmount = makeConversion(fromCurrency, toCurrency, amount);
+
+                    moneyInput1.setText(Double.toString(convertedAmount));
+
                     syncManager.editingText = false;
                 }
             }
@@ -78,8 +114,36 @@ public class MainActivity extends AppCompatActivity implements AsyncResponse {
             JSONObject jsonOutput = new JSONObject(output);
             exchangeRates = jsonOutput.getJSONObject("rates");
             System.out.println("1 Euro is $" + exchangeRates.getDouble("USD"));
+
+            // Also update the spinner
+            Iterator<String> keys = exchangeRates.keys();
+            List<String> currencies = new ArrayList<>();
+
+            while (keys.hasNext()) {
+                currencies.add(keys.next());
+            }
+            System.out.println(currencies);
+
+            // AdapterView is a view that holds data; ArrayAdapter provides data as an array
+            ArrayAdapter<String> arrayAdapter1 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, currencies);
+            arrayAdapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+            ArrayAdapter<String> arrayAdapter2 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, currencies);
+            arrayAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+            spinner1.setAdapter(arrayAdapter1);
+            spinner2.setAdapter(arrayAdapter1);
+
         } catch (JSONException e) {
             exchangeRates = null;
+        }
+    }
+
+    public double makeConversion(String fromCurrency, String toCurrency, double amount) {
+        try {
+            return (amount / exchangeRates.getDouble(fromCurrency)) * exchangeRates.getDouble(toCurrency);
+        } catch (JSONException e) {
+            return 1.0;
         }
     }
 }
